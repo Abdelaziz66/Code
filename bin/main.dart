9006bffdf013873f93ceaddd10f9896fcc057c7e
Abdelaziz66 <3bdel3zizelsayed123@gmail.com>
2025-01-12 13:01:45 +0200

    commit

--- bin/main.dart
+++ bin/main.dart
@@ -1,70 +1,9 @@
 
-import 'algorithm/binary_search.dart';
-import 'design_pattern/creation/proto_type.dart';
-import 'design_pattern/creation/single_tone.dart';
-import 'data_structure/linked_list.dart';
-import 'algorithm/selection_sort.dart';
-import 'design_pattern/creation/builder.dart';
-import 'design_pattern/creation/factory_method.dart';
-import 'design_pattern/creation/abstract_factory.dart';
 import 'problem_solving_leet_code/392.dart';
-import 'design_pattern/structural/proxy.dart';
-import 'design_pattern/structural/decorator.dart';
-import 'design_pattern/structural/adapter.dart';
-import 'design_pattern/structural/facade.dart';
-import 'design_pattern/structural/fly_weight.dart';
-import 'design_pattern/behaviolar/chain_of_responsibility.dart';
 
-void main(List<String> arguments)async {
 
-  // callSingleTone();
-  // callProtoType();
-  // callBinarySearch();
-  // callLinkedList();
-  // callSelectionSort();
-  // callBuilder();
-  // callFactoryMethod();
-  // callAbstractFactory();
- // print( Solution.mergeAlternately('ab', 'pqrs'));
- // print( Solution.canPlaceFlowers([1,0,0,0,1], 2));
- // callProxyPattern();
- // callDecoratorPattern();
- // print( Solution.reverseVowels('IceCreAm'));
- // print( Solution.productExceptSelf([1,2,3,4]));
- // callAdapterPattern();
- // callFacadePattern();
- // callFlyWeight();
- // callChainOfResponsibility();
- // print( Solution.increasingTriplet([5,1,2,0,3,]));
- // B b=B();
-  print(Solution.isSubsequence('abc','ahbgdc'
+void main(List<String> arguments) async {
 
-  ));
+  print(Solution.isSubsequence('abc', 'ahbgdc'));
 
 }
-
-
-
-
-
-
-
-
-
-
-
-// class A {
-//  int i=1;
-//   A() {
-//   print("A");
-//  }
-// }
-//
-// class B extends A {
-//
-//   B() {
-//   print("B");
-//  }
-//
-//
-// }
